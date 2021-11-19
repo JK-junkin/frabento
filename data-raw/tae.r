2d90be5ee3c014cf2b60fb6be06208bd8cab2dff
JK-junkin <kinoshitaj@affrc.go.jp>
2021-11-19 23:56:35 +0900

    :new: To use Try And Error

---
+++ data-raw/tae.r
@@ -0,0 +1,17 @@
+
+# devtools::install(here::here())
+devtools::load_all(here::here())
+ls("package:frabento")
+
+mixFonts(paste0(0:1, "歳"))
+
+mixFonts("\nシラスを除く\nカタクチイワシ0歳魚")
+
+needs(tidyverse, fontregisterer, ggtext)
+
+devtools::check()
+
+stringi::stri_escape_unicode("シラス込み\n0歳魚")
+# [1] "\\u30b7\\u30e9\\u30b9\\u8fbc\\u307f\\n0\\u6b73\\u9b5a"
+stringi::stri_unescape_unicode("シラス込み\n0歳魚")
+# [1] "シラス込み\n0歳魚"
