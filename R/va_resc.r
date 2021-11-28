83bd1dbeb99560bb90feb988b260856d8a89d5fc
JK-junkin <kinoshitaj@affrc.go.jp>
2021-11-28 23:09:43 +0900

    :white_check_mark::zap: Add tests and improvement

--- R/va_resc.r
+++ R/va_resc.r
@@ -1,15 +1,15 @@
-#' @title Values and 2nd Axis Rescaler
+#' @title Values and second Axis RESCaler
 #' @description 2軸グラフを描く時は左軸に値をスケーリングし, 右軸は目盛り数値を調整する.
 #' @param v Values to plot. 描画したい数値のベクトル.
-#' @param lim1 Limit(s) of 1st (left-hand) y-axis. 左軸に対してplotしたい変数の範囲 (要素数2) またはvector (要素数3以上).
-#' vectorを指定した場合は range(lim1) となる. また, 正数であれば最大値のみの指定も可能 (最小値は0となる).
-#' NULLの場合は range(v) となる. Default: NULL
-#' @param lim2 Limit(s) of 2nd (right-hand) y-axis. 右軸に対してplotしたい変数の範囲 (要素数2) またはvector (要素数3以上).
-#' vectorを指定した場合は range(lim2) となる. また, 正数であれば最大値のみの指定も可能 (最小値は0となる).
-#' NULLの場合は range(v) となる. Default: NULL
+#' @param lh_lim Limit(s) of 1st (left-hand) y-axis. 左軸の範囲 (要素数2) またはvector (要素数3以上).
+#' vectorを指定した場合は range(lh_lim) となる. また, 正数であれば最大値, 負数であれば最小値のみの指定も可能 (もう一方の値は0となる).
+#' NULLの場合は range(v, na.rm = TRUE) となる. Default: NULL
+#' @param rh_lim Limit(s) of 2nd (right-hand) y-axis. 右軸の範囲 (要素数2) またはvector (要素数3以上).
+#' vectorを指定した場合は range(rh_lim) となる. また, 正数であれば最大値のみの指定も可能 (最小値は0となる).
+#' NULLの場合は range(v, na.rm = TRUE) となる. Default: NULL
 #' @param scale_which A string. 右軸 (2nd y-axis) に対して描画したい変数を左軸にスケーリングする場合は`var`,
-#' 右軸の目盛りを変換する場合は`axis`. Default: "var".
-#' @param return_lims Logical. 変換後の数値の代わりに範囲を標準 (message) 出力する, Default: FALSE
+#' 右軸の目盛り値をスケーリングする場合は`axis`. Default: "var".
+#' @param return_lims Logical. 変換後の数値の代わりに範囲を出力する (matrix型), Default: FALSE
 #' @return 変換後の数値
 #' @details See example.
 #' @references
@@ -36,46 +36,43 @@
 #' }
 #' @rdname va_resc
 #' @export
-va_resc <- function(v, lim1 = NULL, lim2 = NULL,
+va_resc <- function(v, lh_lim = NULL, rh_lim = NULL,
                     scale_which = "var", return_lims = FALSE) {
-    if (is.null(lim1)) lim1 <- range(v, na.rm = TRUE)
-    if (is.null(lim2)) lim2 <- range(v, na.rm = TRUE)
+    if (is.null(lh_lim)) lh_lim <- range(v, na.rm = TRUE)
+    if (is.null(rh_lim)) rh_lim <- range(v, na.rm = TRUE)
 
-    if (all(length(lim1) == 1, lim1 < 0)) stop("If the maximum value is negative, 'lim1' must include no less than two values.")
-    if (all(length(lim2) == 1, lim2 < 0)) stop("If the maximum value is negative, 'lim2' must include no less than two values.")
+    if (all(length(lh_lim) == 1, lh_lim == 0)) stop("When assigning A value to 'lh_lim', it should not be equal to zero ",
+                                                "because the returned value will be all zeros.")
+    if (all(length(rh_lim) == 1, rh_lim == 0)) stop("When assigning A value to 'rh_lim', it should not be equal to zero ",
+                                                "because the returned value will be Inf.")
 
-    if (all(length(lim1) == 1, lim1 == 0)) warning("'lim1' should be no less than ", lim1,
-                                                   " when assigning a value. The return value will be all zeros.")
-    if (all(length(lim2) == 1, lim2 == 0)) warning("'lim2' should be no less than ", lim2,
-                                                   " when assigning a value. The return value will be all zeros.")
+    if (all(length(lh_lim) == 1, lh_lim != 0)) lh_lim <- c(0, lh_lim) 
+    if (all(length(rh_lim) == 1, rh_lim != 0)) rh_lim <- c(0, rh_lim) 
 
-    if (all(length(lim1) == 1, lim1 >= 0)) lim1 <- c(0, lim1) 
-    if (all(length(lim2) == 1, lim2 >= 0)) lim2 <- c(0, lim2) 
+    if (length(lh_lim) > 2) lh_lim <- range(lh_lim, na.rm = TRUE)
+    if (length(rh_lim) > 2) rh_lim <- range(rh_lim, na.rm = TRUE)
 
-    if (length(lim1) > 2) lim1 <- range(lim1, na.rm = TRUE)
-    if (length(lim2) > 2) lim2 <- range(lim2, na.rm = TRUE)
+    if (lh_lim[1] > lh_lim[2]) lh_lim <- rev(lh_lim)
+    if (rh_lim[1] > rh_lim[2]) rh_lim <- rev(rh_lim)
 
-    if (lim1[1] >= lim1[2]) lim1 <- rev(lim1)
-    if (lim2[1] >= lim2[2]) lim2 <- rev(lim2)
+#     if (any(min(v, na.rm = TRUE) < range(c(lh_lim, rh_lim))[1],
+#             max(v, na.rm = TRUE) > range(c(lh_lim, rh_lim))[2])) {
+#         warning("One or more values are out of bounds. Confirm lh_lim a/o rh_lim.")
+#     }
 
-    if (any(min(v, na.rm = TRUE) < range(c(lim1, lim2))[1],
-            max(v, na.rm = TRUE) > range(c(lim1, lim2))[2])) {
-        warning("One or more values are out of bounds. Confirm lim1 a/o lim2.")
-    }
+    if (return_lims) return(rbind(lh_lim, rh_lim))
 
-    if (return_lims) return(rbind(lim1, lim2))
-
-    y1_range  <- lim1[2] - lim1[1]
-    y2_range  <- lim2[2] - lim2[1]
+    y1_range  <- lh_lim[2] - lh_lim[1]
+    y2_range  <- rh_lim[2] - rh_lim[1]
 
     if (scale_which == "var") { # 右軸にプロットする変数を想定
-        to_zero   <- v - lim2[1]
+        to_zero   <- v - rh_lim[1]
         scaled    <- to_zero * (y1_range / y2_range)
-        from_zero <- scaled + lim1[1]
+        from_zero <- scaled + lh_lim[1]
     } else {                    # 左軸を右軸にスケーリング
-        to_zero   <- v - lim1[1]
+        to_zero   <- v - lh_lim[1]
         scaled    <- to_zero * (y2_range / y1_range)
-        from_zero <- scaled + lim2[1]
+        from_zero <- scaled + rh_lim[1]
     }
     return(from_zero)
 }
--- tests/testthat/test-va_resc.R
+++ tests/testthat/test-va_resc.R
@@ -1,16 +1,48 @@
 library(frabento)
+# devtools::load_all(here::here())
 
 test_that("va_resc", {
-    vl <- 1:5
-    vr <- seq(20, 100, by = 20)
+    rp <- c(5, 15)
+    rpn <- c(-15, -5, 5, 15)
+    rn <- c(-15, -5)
 
-    expect_error(va_resc(vl, -10, 100))
-    expect_error(va_resc(vl, 10, -100))
-    expect_warning(va_resc(vl, 0, 100))
-    expect_warning(va_resc(vl, c(-10, -5), c(0, -100)))
+    alp <- c(0, 1)
+    alpn <- c(-1, 1)
+    aln <- c(-1, 0)
 
-    expect_equal(va_resc(vr, 10, 100), vr/(100/10))
-    expect_equal(va_resc(vl, 10, 100, "axis"), vl/(10/100))
+    arp <- c(0, 10)
+    arpn <- c(-10, 10)
+    arn <- c(-10, 0)
 
-#     expect_equal(va_resc(vv, c(-10, 0), 100, "var"), vv/(100))
+    # 右が正, 左が正
+    expect_equal(va_resc(va_resc(rp, alp, arp, "var"), alp, arp, "axis"), rp)
+    
+    # 右が正負, 左が正
+    expect_equal(va_resc(va_resc(rpn, alp, arpn, "var"), alp, arpn, "axis"), rpn)
+
+    # 右が負, 左が正
+    expect_equal(va_resc(va_resc(rn, alp, arn, "var"), alp, arn, "axis"), rn)
+    
+    # 右が正, 左が正負
+    expect_equal(va_resc(va_resc(rp, alpn, arp, "var"), alpn, arp, "axis"), rp)
+   
+    # 右が正負, 左が正負
+    expect_equal(va_resc(va_resc(rpn, alpn, arpn, "var"), alpn, arpn, "axis"), rpn)
+    
+    # 右が負, 左が正負
+    expect_equal(va_resc(va_resc(rn, alpn, arn, "var"), alpn, arn, "axis"), rn)
+    
+    # 右が正, 左が負
+    expect_equal(va_resc(va_resc(rp, aln, arp, "var"), aln, arp, "axis"), rp)
+    
+    # 右が正負, 左が負
+    expect_equal(va_resc(va_resc(rpn, aln, arpn, "var"), aln, arpn, "axis"), rpn)
+    
+    # 右が負, 左が負
+    expect_equal(va_resc(va_resc(rn, aln, arn, "var"), aln, arn, "axis"), rn)
+
+    expect_error(va_resc(rp, 0, 100))
+    expect_error(va_resc(rp, 1, 0))
+#     expect_warning(va_resc(vl, 0, 100))
+#     expect_warning(va_resc(vl, c(-10, -5), c(0, -100)))
 })
