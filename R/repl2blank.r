34364d0d3fe5089556be8e926287c8fd264552ad
JK-junkin <kinoshitaj@affrc.go.jp>
2021-12-02 01:29:28 +0900

    :broom: Improve title and add examples

--- R/repl2blank.r
+++ R/repl2blank.r
@@ -1,5 +1,5 @@
-#' @title 軸ラベルを一定間隔でブランクに置き換える関数
-#' @description 当初はggplot作図の際に軸ラベルの一部を空白にすることで minor break のようにみせかけるために作成.
+#' @title Replace string elements to blank at a certain interval
+#' @description ggplot作図の際に軸ラベルの一部を空白にすることで minor break のようにみせかける.
 #' @param vec A character or an numeric vector. ラベルにする文字列/数値ベクトル
 #' @param nskip 何個置きに空白文字にするか, Default: 1.
 #' @param first 1個目の値を空白にする, Default: FALSE. TRUEで強制的に空白にする. inverse = TRUE の時はTRUEで強制的に値を与える.
@@ -38,7 +38,7 @@ repl2blank <-
     vec
 }
 
-#' @title 軸ラベルを一定間隔でブランクに置き換える関数を返す
+#' @title Label character vector with blanks at a certain interval
 #' @description ggplot作図の際に軸ラベルの一部を空白にすることで minor break のようにみせかける.
 #' @param nskip 何個置きに空白文字にするか, Default: 1.
 #' @param first 1個目の値を空白にする, Default: FALSE. TRUEで強制的に空白にする. inverse = TRUE の時はTRUEで強制的に値を与える.
@@ -51,7 +51,15 @@ repl2blank <-
 #' @examples
 #' \dontrun{
 #' if(interactive()){
-#'  #EXAMPLE1
+#' library(dplyr)
+#' library(tibble)
+#' library(ggplot2)
+#' 
+#' tibble::tibble(year =  -6000:1979, haba = treering) %>%
+#'     ggplot(aes(x = year, y = haba)) +
+#'     geom_line(size = 0.2) +
+#'     scale_x_continuous(breaks = seq(-6000, 2000, by = 200),
+#'                        labels = label_repl2blank(nskip = 4))
 #'  }
 #' }
 #' @rdname label_repl2blank
