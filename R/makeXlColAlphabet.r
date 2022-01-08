1f512fab973d8dadf41e6c9cf2fe7d5151d2800c
JK-junkin <j.kinoshita8802@gmail.com>
2022-01-09 01:40:46 +0900

    :elephant: Big update

--- R/makeXlColAlphabet.r
+++ R/makeXlColAlphabet.r
@@ -3,7 +3,7 @@
 #' @param nchar A number of combined characters, Default: 2
 #' @param len Length (size) of the output vector, Default: NULL
 #' @param start Index or character at start, Default: 1 (is 'A'). `start` larger than `len` will return NA.
-#' @param fl show only first and last character, Default: FALSE
+#' @param print_all displays all characters to the console if TRUE. When FALSE (default), showing only the first and last characters with column numbers.
 #' @return A character vector.
 #' @details No description.
 #' @examples
@@ -11,7 +11,7 @@
 #' if(interactive()){
 #' makeXlColAlphabet(nchar = 1)
 #' makeXlColAlphabet(len = 10, start = 3)
-#' makeXlColAlphabet(len = length(1978:2020), start = "B", fl = TRUE)
+#' makeXlColAlphabet(len = length(1978:2020), start = "B", print_all = TRUE)
 #'  }
 #' }
 #' @seealso 
@@ -22,32 +22,45 @@
 #' @importFrom dplyr case_when
 #' @importFrom utils head tail
 #' @export 
-makeXlColAlphabet <- function(nchar = 2L, len = NULL, start = 1L, fl = FALSE) {
+makeXlColAlphabet <- function(nchar = 2L, len = NULL, start = 1L,
+                              print_all = FALSE) {
+
+    if (nchar <= 0) stop("'nchar' should be greater than or equal to (GTE) 1.")
+    if (nchar > 3) stop("'nchar' greater than (GT) 3 is not supported.")
+    if (is.character(start) & nchar < nchar(start)) {
+        stop("Preset 'nchar = '", nchar, " is exceeded by `nchar(start)`.\n",
+             "Please confirm the 'start' string.")
+    }
 
     i <- NULL # for R CMD CHECK: no visible binding for global variable
     x <- foreach::foreach(i = seq_len(nchar), .combine = "c") %do% {
         do.call(paste0, expand.grid(rep(list(LETTERS), i))[rev(seq_len(i))])
     }
 
-    if(nchar <= 0) stop("'nchar' should be greater than or equal to (GTE) 1.")
-    if(nchar > 3) stop("'nchar' greater than (GT) 3 is not supported.")
+    if (is.character(start)) {
+        start <- stringr::str_which(x, paste0("^", toupper(start), "$"))
+    }
 
-    if(is.character(start)) start <- stringr::str_which(toupper(start), x)
-    if(start < 1) stop("'start' should be greater than 0.")
-
-    if(is.null(len)) {
+    if (is.null(len)) {
         len <- dplyr::case_when(nchar == 1 ~ 26^1 - start + 1,
                                 nchar == 2 ~ 26^1 + 26^2 - start + 1,
                                 nchar == 3 ~ 26^1 + 26^2 + 26^3 - start + 1)
     }
 
+    if (start < 1) {
+        stop("'start' should be greater than 0.")
+    } else if (len < 1) {
+        stop("'start' should be less than ", len + start,
+             " when 'nchar' is ", nchar, ".")
+    }
+
     res <- x[start:(len + start - 1L)]
-    if(fl) {
-        return(c(head(res, n = 1), tail(res, n = 1)))
-    } else {
-        message(paste(head(res, n = 2), collapse = ", "), ", ..., ",
-                paste(tail(res, n = 2), collapse = ", "), " are gonna return.")
-        return(res)
+    if (!print_all) {
+        firlas <- unique(res[c(1, length(res))])
+        names(firlas) <- as.character(unique(c(start, start + len - 1)))
+        firlas
+    } else if (print_all) {
+        res
     }
 }
 
