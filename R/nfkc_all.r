ba8f6055e6cffb441465973304f44bdf5ffd4e23
JK-junkin <kinoshitaj@affrc.go.jp>
2023-04-17 08:31:33 +0900

    :bug: From [:space:] to [:blank:] in nfkc_all()

--- R/nfkc_all.r
+++ R/nfkc_all.r
@@ -21,11 +21,11 @@
 #'  tbl <- tibble::tibble(`列１（トン）` = "1.2\r\n1.3",
 #'                        `列2 (kg) ` = " 2.2\n 2.3")
 #'  nfkc_all(tbl)
-#'  # remove white-spaces from all column names
+#'  # remove white-spaces (blanks) from all column names
 #'  nfkc_all(tbl, remove_space_colnames = TRUE)
-#'  # remove white-spaces from all contents
+#'  # remove white-spaces (blanks) from all contents
 #'  nfkc_all(tbl, remove_space_contents = TRUE)
-#'  # remove all white-spaces from .data
+#'  # remove all white-spaces (blanks) from .data
 #'  nfkc_all(tbl, remove_space_colnames = TRUE, remove_space_contents = TRUE)
 #' @rdname nfkc_all
 #' @export 
@@ -42,12 +42,12 @@ nfkc_all <- function(.data, remove_space_colnames = FALSE,
 
     if(remove_space_colnames) {
         out <- 
-            dplyr::rename_all(out, ~ stringr::str_remove_all(.x, "[:space:]"))
+            dplyr::rename_all(out, ~ stringr::str_remove_all(.x, "[:blank:]"))
     }
 
     if(remove_space_contents) {
         out <- 
-            dplyr::mutate_all(out, ~ stringr::str_remove_all(.x, "[:space:]"))
+            dplyr::mutate_all(out, ~ stringr::str_remove_all(.x, "[:blank:]"))
     }
 
     class(out) <- cls
